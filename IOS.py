208c976a9697e4ec7649d8e38e1f7fefa5e2f55e
Alex Munoz <alex@napend.com>
2020-09-06 16:00:12 -0400

    Removed ASA, added readme, added date and time and hostname to file name

--- IOS.py
+++ IOS.py
@@ -1,12 +1,15 @@
+from datetime import datetime
+from datetime import date
 from netmiko import ConnectHandler
 import getpass
+
+now = datetime.now()
+dt_string = now.strftime("%m-%d-%Y_%H-%M")
 
 host = input("IP: ")
 username = input("Username: ")
 password = getpass.getpass("Password: ")
 enableSecret = getpass.getpass("Enable Secret: ")
-
-backupFile = open("Output-Configs/" + host + ".txt", "w+")
 
 cisco_ios = {
     'device_type': 'cisco_ios',
@@ -20,6 +23,13 @@ net_connect  = ConnectHandler(**cisco_ios)
 net_connect.enable()
 
 output = net_connect.send_command("show running-config")
+result = net_connect.send_command("show run | in hostname")
+result = result.split()
+hostname = result[1]
+
+backupFile = open("Output-Configs/" + hostname + ".txt", "w+")
 backupFile.write(output)
-#print(output)
-print("Outputted to running-config.txt!")
+
+fileName = hostname + "_" + dt_string
+
+print("Outputted to " + fileName + ".txt!")
---
+++ README.md
@@ -0,0 +1,5 @@
+Steps to run:
+
+1. Run `pip install netmiko`
+2. Create folder `Output-Configs`
+3. Run `python3 IOS.py`
