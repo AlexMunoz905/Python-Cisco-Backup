a735c0ba99282026d052fd97533b6955202c0424
Alexander Muñoz <alex.munoz905@gmail.com>
2020-10-31 23:33:58 -0400

    Add files via upload

---
+++ run.py
@@ -0,0 +1,40 @@
+from datetime import datetime
+from datetime import date
+from netmiko import ConnectHandler
+import getpass
+import os
+
+if not os.path.exists('Output-Configs'):
+    os.makedirs('Output-Configs')
+
+now = datetime.now()
+dt_string = now.strftime("%m-%d-%Y_%H-%M")
+
+host = input("IP: ")
+username = input("Username: ")
+password = getpass.getpass("Password: ")
+enableSecret = getpass.getpass("Enable Secret: ")
+
+cisco_ios = {
+    'device_type': 'cisco_ios',
+    'host': host,
+    'username': username,
+    'password': password,
+    'secret': enableSecret,
+}
+
+net_connect  = ConnectHandler(**cisco_ios)
+net_connect.enable()
+
+output = net_connect.send_command("show running-config")
+result = net_connect.send_command("show run | in hostname")
+result = result.split()
+hostname = result[1]
+
+fileName = hostname + "_" + dt_string
+
+backupFile = open("Output-Configs/" + fileName + ".txt", "w+")
+backupFile.write(output)
+
+
+print("Outputted to " + fileName + ".txt!")
