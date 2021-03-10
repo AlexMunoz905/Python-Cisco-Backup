8a72d2cf6b81a24a44e51bf9f9d02d3bce0cb389
Alex Munoz <alex@napend.com>
2021-03-10 00:34:41 +0000

    Pings IPs to check if they are down before trying to save their config. If down, outputs to text file with date and time.

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
-# Imports all the crucial items.
 # All pre-installed besides Netmiko.
 from csv import reader
 from datetime import date, datetime
 from netmiko import ConnectHandler
+from ping3 import ping, verbose_ping 
 import getpass
 import os
 
@@ -29,9 +29,9 @@ def get_saved_config(host, username, password, enable_secret):
     # Gets the running configuration.
     output = net_connect.send_command("show running-config")
     # Gets and splits the hostname for the output file name.
-    hostname = net_connect.send_command("show run | in hostname")
+    hostname = net_connect.send_command("show ver | i uptime")
     hostname = hostname.split()
-    hostname = hostname[1]
+    hostname = hostname[0]
     # Creates the file name, which is the hostname, and the date and time.
     fileName = hostname + "_" + dt_string
     # Creates the text file in the Output-Configs folder with the special name, and writes to it.
@@ -45,19 +45,36 @@ def manual_option():
     username = input("Username: ")
     password = getpass.getpass("Password: ")
     enable_secret = getpass.getpass("Enable Secret: ")
-    get_saved_config(host, username, password, enable_secret)
+    #get_saved_config(host, username, password, enable_secret)
+    ip = host
+    ip_ping = ping(ip)
+    if ip_ping == None:
+        fileName = "downDevices_" + dt_string + ".txt"
+        downDeviceOutput = open("Output-Configs/" + fileName, "a")
+        downDeviceOutput.write(str(ip) + "\n")
+    else:
+        get_saved_config(ip, username, password, enable_secret)
 
-# Gets the CSV file name, and grabs the information from it.
 def csv_option():
     csv_name = input("\nWhat is the name of your CSV file?: ")
-    with open(csv_name, 'r') as read_obj:
+    with open('backups.csv', 'r') as read_obj:
         csv_reader = reader(read_obj)
         list_of_rows = list(csv_reader)
+        #print(list_of_rows)
         rows = len(list_of_rows)
+        #print(rows)
         while rows >= 2:
             rows = rows - 1
-            get_saved_config(list_of_rows[rows][0], list_of_rows[rows][1], list_of_rows[rows][2], list_of_rows[rows][3])
-
+            #print(list_of_rows[rows][0])
+            ip = list_of_rows[rows][0]
+            ip_ping = ping(ip)
+            if ip_ping == None:
+                fileName = "downDevices_" + dt_string + ".txt"
+                downDeviceOutput = open("Output-Configs/" + fileName, "a")
+                downDeviceOutput.write(str(ip) + "\n")
+                # print(str(ip) + " IS DOWN!")
+            else:
+                get_saved_config(list_of_rows[rows][0], list_of_rows[rows][1], list_of_rows[rows][2], list_of_rows[rows][3])
 # Asks the user what option they are going to use.
 print("\n1. Input information your self.")
 print("2. Input information directly from CSV file.\n")
@@ -75,3 +92,4 @@ if choice == "1":
 elif choice == "2":
     # Runs the whole CSV option.
     csv_option()
+
