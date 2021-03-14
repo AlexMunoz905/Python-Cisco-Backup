bced3e02777a911b017421a28ed099fc78b79299
Stefano <stefano@nerv.it>
2021-03-14 16:58:06 +0100

    Add files via upload

---
+++ i_run.py
@@ -0,0 +1,97 @@
+# All pre-installed besides Netmiko.
+from csv import reader
+from datetime import date, datetime
+from netmiko import ConnectHandler
+from ping3 import ping, verbose_ping 
+import getpass
+import os
+import sys
+
+sys.tracebacklimit = 0
+
+# Checks if the folder exists, if not, it creates it.
+if not os.path.exists('backup-config'):
+    os.makedirs('backup-config')
+
+# Current time and formats it to the North American time of Month, Day, and Year.
+now = datetime.now()
+dt_string = now.strftime("%m-%d-%Y_%H-%M")
+
+# Gives us the information we need to connect.
+def get_saved_config(host, username, password, enable_secret):
+    cisco_ios = {
+        'device_type': 'cisco_ios',
+        'host': host,
+        'username': username,
+        'password': password,
+        'secret': enable_secret,
+    }
+    # Creates the connection to the device.
+    net_connect = ConnectHandler(**cisco_ios)
+    net_connect.enable()
+    # Gets the running configuration.
+    output = net_connect.send_command("show run")
+    # Gets and splits the hostname for the output file name.
+    hostname = net_connect.send_command("show ver | i uptime")
+    hostname = hostname.split()
+    hostname = hostname[0]
+    # Creates the file name, which is the hostname, and the date and time.
+    fileName = hostname + "_" + dt_string
+    # Creates the text file in the backup-config folder with the special name, and writes to it.
+    backupFile = open("backup-config/" + fileName + ".txt", "w+")
+    backupFile.write(output)
+    print("Outputted to " + fileName + ".txt!")
+
+# Asks for the IP, Username, Password, and Enable Secret, and passes it along to the get_saved_config function.
+def manual_option():
+    host = input("\nIP: ")
+    username = input("Username: ")
+    password = getpass.getpass("Password: ")
+    enable_secret = getpass.getpass("Enable Secret: ")
+    #get_saved_config(host, username, password, enable_secret)
+    ip = host
+    ip_ping = ping(ip)
+    if ip_ping == None:
+        fileName = "downDevices_" + dt_string + ".txt"
+        downDeviceOutput = open("backup-config/" + fileName, "a")
+        downDeviceOutput.write(str(ip) + "\n")
+    else:
+        get_saved_config(ip, username, password, enable_secret)
+
+def csv_option():
+    csv_name = input("\nWhat is the name of your CSV file?: ")
+    with open(csv_name, 'r') as read_obj:
+        csv_reader = reader(read_obj)
+        list_of_rows = list(csv_reader)
+        #print(list_of_rows)
+        rows = len(list_of_rows)
+        #print(rows)
+        while rows >= 2:
+            rows = rows - 1
+            #print(list_of_rows[rows][0])
+            ip = list_of_rows[rows][0]
+            ip_ping = ping(ip)
+            if ip_ping == None:
+                fileName = "downDevices_" + dt_string + ".txt"
+                downDeviceOutput = open("backup-config/" + fileName, "a")
+                downDeviceOutput.write(str(ip) + "\n")
+                print(str(ip) + " is down!")
+            else:
+                get_saved_config(list_of_rows[rows][0], list_of_rows[rows][1], list_of_rows[rows][2], list_of_rows[rows][3])
+# Asks the user what option they are going to use.
+print("\n1. Input information your self.")
+print("2. Input information directly from CSV file.\n")
+choice = input("Please pick an option: ")
+
+# This basically runs the whole file.
+if choice == "1":
+    # Figures out how many hosts the user is going to manually input.
+    how_many = input("How many hosts?: ")
+    how_many = int(how_many)
+    i = how_many
+    while i >= 1:
+        manual_option()
+        i = i - 1
+elif choice == "2":
+    # Runs the whole CSV option.
+    csv_option()
