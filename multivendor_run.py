42c12443618765e16a35c3fa4249b173960e95a8
Stefano <stefano@nerv.it>
2021-08-30 22:39:15 +0200

    Update multivendor_run.py

--- multivendor_run.py
+++ multivendor_run.py
@@ -59,7 +59,7 @@ def get_saved_config_j(host, username, password,):
     # Gets the running configuration.
     output = net_connect.send_command("show conf | display set")
     # Gets and splits the hostname for the output file name.
-    hostname = net_connect.send_command("show ver | i hostname")
+    hostname = net_connect.send_command("show ver | grep hostname")
     hostname = hostname.split()
     hostname = hostname[1]
     # Creates the file name, which is the hostname, and the date and time.
