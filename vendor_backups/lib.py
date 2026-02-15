3f91b0bdc0fda165ed4b729f59374ef4a35aa4f8
Joshua Schmidlkofer <joshland@ashbyte.com>
2026-02-15 01:08:23 -0800

    update: fix filename

--- vendor_backups/lib.py
+++ vendor_backups/lib.py
@@ -2,7 +2,7 @@ import os
 
 def write_backup(fileName: str, output: str):
     # Creates the text file in the backup-config folder with the special name, and writes to it.
-    backup_file = os.path.join("backup-config", fileName)
+    backup_file = os.path.join("backup-config", f"{fileName}.txt")
     with open(backup_file, "w") as backupFile:
         backupFile.write(output)
         pass
